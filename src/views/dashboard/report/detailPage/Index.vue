e82dd8960f28be2d4184b0d770875f69372be3a8
Asus <mbadox45@gmail.com>
2025-01-02 08:43:03 +0700

    update ukuran card, text, dan penyesuaian gambar

--- src/views/dashboard/report/detailPage/Index.vue
+++ src/views/dashboard/report/detailPage/Index.vue
@@ -1,8 +1,22 @@
 <script setup>
+import { onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
 const router = useRouter();
+
+onMounted(() => {
+    funcCondition();
+});
+
+const funcCondition = () => {
+    const query = route.query.component;
+    if (query == null || query == '') {
+        router.push({ path: '/not-found' });
+    } else {
+        console.log(query);
+    }
+};
 </script>
 
 <template>
