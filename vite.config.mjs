9862fa9288b52bb77352b8c996ac26f81933a6d4
TomyLingga <tomy.troller@gmail.com>
2025-01-03 11:29:29 +0700

    dah bisa chartnya

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,6 +8,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
+        include: ['apexcharts', 'vue3-apexcharts'],
         noDiscovery: true
     },
     plugins: [
