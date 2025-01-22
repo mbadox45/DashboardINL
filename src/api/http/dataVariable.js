2dc378b5431a96336475ea6cfcbdbcbbb43456ba
TomyLingga <tomy.troller@gmail.com>
2025-01-22 14:47:13 +0700

    ganti var

--- src/api/http/dataVariable.js
+++ src/api/http/dataVariable.js
@@ -1,5 +1,5 @@
 // Public Access
-export const URL_WEB = 'http://localhost:8481/';
+export const URL_WEB = 'http://192.168.1.223:8481/';
 export const URL_API = 'http://36.92.181.10:30008/api/';
 export const URL_API_INL_EDGE = 'http://36.92.181.10:9009/api/';
 // export const URL_API_INL_EDGE = 'http://localhost:8000/api/';
